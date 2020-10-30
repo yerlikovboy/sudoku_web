b44a17ed7f164d8f71eb0683e41376fa4fc19825
Ali Guler <maguler@gmail.com>
2020-10-30 06:03:40 -0500

    restructure project, fix starting index bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import "./Board.css";
+import "./css/Board.css";
 import Board from "./Board.js";
+import GameController from "./Controls.js"
 
-export default function App(props) { 
-      return (
-      <div className="game">
-        <div className="title">9x9 Sudoku Board</div>
-        <Board puzzle={props.puzzle}/>
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      puzzle: props.puzzle,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="game">
+          <div className="title">9x9 Sudoku Board</div>
+          <Board puzzle={this.state.puzzle} />
+        </div>
+        <div className="game-controls">
+          <GameController />
+        </div>
       </div>
-    );
+    )
+  }
 }
+
+export default App;
